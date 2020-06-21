80b9d4d186f7d19a2e2e3fcc3050f54346817f00
FMW14 <fmw14@yandex.ru>
2020-06-21 16:50:15 +0300

    group complete

--- src/main/java/com/vtb/javacourses/lesson2/Group.java
+++ src/main/java/com/vtb/javacourses/lesson2/Group.java
@@ -4,28 +4,44 @@ public class Group {
     private String name;
     private Employee[] employees = new Employee[10];
 
-    public Group() {
-    }
-
     public Group(String name) {
         this.name = name;
     }
 
-    public void addEmployee(Employee emp) {
-
+    public void addEmployee(Employee newEmp) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                employees[i] = newEmp;
+                break;
+            }
+        }
     }
 
     public void removeEmployeeByIndex(Integer a) {
-
+        if (a > 0 && a < employees.length) {
+            employees[a] = null;
+        }
     }
 
     public void removeAllEmployees() {
-
+        employees = new Employee[10];
     }
 
     public void printEmployees() {
-        for (Employee employee : employees) {
-            employee.printInfo();
+        System.out.println("Group: " + name);
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                System.out.print(i + ". ");
+                employees[i].printInfo();
+            }
         }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
