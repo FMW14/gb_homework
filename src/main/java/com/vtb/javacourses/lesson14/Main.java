6d4b470dc59bf74ed7c325c1c48134b4ac48f21b
FMW14 <fmw14@yandex.ru>
2020-07-18 22:19:06 +0300

    task2 done

--- src/main/java/com/vtb/javacourses/lesson14/Main.java
+++ src/main/java/com/vtb/javacourses/lesson14/Main.java
@@ -1,47 +1,90 @@
 package com.vtb.javacourses.lesson14;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String FILENAME1 = ".\\data1.txt";
-        String readed = readFile(FILENAME1);
-        System.out.println(readed);
+        String FILENAME1 = "./src/main/resources/lesson14/task1/data1.txt";
+        File file = new File(FILENAME1);
 
-        int result = getNumberOfSequence(readed, "wer");
-        System.out.println("Number = " + result);
+//        String readed = readFile(file);
+//        System.out.println(readed);
+
+//        int result = getNumberOfSequence(readFile(file).toString(), "wer");
+        getNumberOfSequence(readFile(file), "wer");
+
+        joinAllFiles(new File("./src/main/resources/lesson14/task2/"));
 
     }
 
     //Task1
-    public static int getNumberOfSequence(String string, String sequence){
-//        char[] stringChars = string.toCharArray();
-//        for (int i = 0; i < stringChars.length; i++) {
-//            System.out.println(i + " " + stringChars[i]);
-//        }
+    public static int getNumberOfSequence(String string, String sequence) {
+        System.out.println("Gotten sequence: " + sequence);
+
         int index = string.indexOf(sequence);
         int count = 0;
         while (index != -1) {
             count++;
-            index = string.indexOf(sequence, index +1);
+            index = string.indexOf(sequence, index + 1);
         }
-//        System.out.println("No of sequence in the string is : " + count);
+
+        System.out.println("Number of sequence entries: " + count);
 
         return count;
     }
 
-    public static String readFile(String fileName) throws IOException {
+    public static String readFile(File file) throws IOException {
         StringBuilder resultReader1 = new StringBuilder();
-        FileInputStream fstream = new FileInputStream(fileName);
+        FileInputStream fstream = new FileInputStream(file);
         BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
         String strLine;
         while ((strLine = br.readLine()) != null) {
             resultReader1.append(strLine).append("\n");
         }
 
+        br.close();
         return resultReader1.toString();
+    }
+
+    //Task2
+    public static File joinAllFiles(File dir) throws IOException {
+        if (dir == null) {
+            throw new FileNotFoundException();
+        }
+        if (!dir.isDirectory()) {
+            throw new FileNotFoundException(dir + " is not directory");
+        }
+        if (!dir.exists()) {
+            throw new FileNotFoundException(dir + " does not exists");
+        }
+
+        if (dir.listFiles() != null) {
+            File[] files = dir.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    int index = name.indexOf(".");
+                    return name.substring(index).equals(".txt");
+                }
+            });
+
+//            File joined = new File(dir.getPath() + "/joined_" + dir.getName() + ".txt");  //NOT WORKING
+            String outputPath = dir.getPath().substring(0, dir.getPath().lastIndexOf(File.separator)) + File.separator +"joined_" + dir.getName() + ".txt";    //separator must always exists in correct path
+            File joined = new File(outputPath);
+            FileOutputStream foStream = new FileOutputStream(joined, false);
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(foStream));
+
+            System.out.println("Starting merge files in " + dir.getPath() + " into " + outputPath);
+            for (File curFile : files) {
+                System.out.println("Joining " + curFile.getName());
+                bufferedWriter.write(readFile(curFile));
+                bufferedWriter.flush();
+            }
+            bufferedWriter.close();
+
+            System.out.println("Files merged");
+            return joined;
+        } else {
+            throw new RuntimeException(dir + " has no files");
+        }
     }
 }
