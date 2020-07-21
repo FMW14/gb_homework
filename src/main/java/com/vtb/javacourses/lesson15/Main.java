fe001718c3da2206bd0d1410827d5580e1446b9c
FMW14 <fmw14@yandex.ru>
2020-07-21 19:27:04 +0300

    task2 done

--- src/main/java/com/vtb/javacourses/lesson15/Main.java
+++ src/main/java/com/vtb/javacourses/lesson15/Main.java
@@ -1,24 +1,35 @@
 package com.vtb.javacourses.lesson15;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Path file1 = Paths.get("./src/main/resources/lesson15/task1.txt");
+        Path path2 = Paths.get("./src/main/resources/lesson15/task2");
 
-        System.out.println(getNumberOfSequence(file1, "we"));
+//        System.out.println(getNumberOfSequence(file1, "we"));
+
+        joinAllFiles(path2);
+//        joinAllFiles(Paths.get("./src/main/resources/lesson15/task4"));
 
     }
 
     //Task1
     public static int getNumberOfSequence(Path path, String sequence) throws IOException {
 //        int cap = 8192;
-        final int cap = sequence.length()*10;
+        final int cap = sequence.length() * 10;
         int resultCount = 0;
         byte[] sequenceByteArr = sequence.getBytes();
 
@@ -30,7 +41,7 @@ public class Main {
         while (bytesRead != -1) {
             buf.flip();
 //            System.out.println("---");
-            while(buf.hasRemaining()){
+            while (buf.hasRemaining()) {
                 boolean isMatch = false;
                 int lastPos = 0;
 
@@ -40,14 +51,14 @@ public class Main {
                 }
 
                 int index = 1;
-                while (isMatch){
-                    if(index == sequenceByteArr.length && isMatch){
+                while (isMatch) {
+                    if (index == sequenceByteArr.length && isMatch) {
                         resultCount++;
                         buf.position(lastPos);
                         break;
                     }
 
-                    if ((char) buf.get() != sequenceByteArr[index]){
+                    if ((char) buf.get() != sequenceByteArr[index]) {
                         isMatch = false;
                         buf.position(lastPos);
                     }
@@ -55,7 +66,7 @@ public class Main {
                     index++;
                 }
 
-                if (buf.limit() - buf.position() < sequenceByteArr.length){
+                if (buf.limit() - buf.position() < sequenceByteArr.length) {
                     break;
                 }
             }
@@ -69,5 +80,43 @@ public class Main {
         return resultCount;
     }
 
+    //Task2
+    public static Path joinAllFiles(Path dir) throws IOException {
+        if (!Files.exists(dir)) {
+            throw new RuntimeException(dir + " doesnt exists");
+        }
+
+        List<Path> pathList = new ArrayList<>();
+        try (Stream<Path> paths = Files.walk(Paths.get(dir.toString()))) {
+
+            pathList = paths
+                    .filter(Files::isRegularFile)
+                    .filter(p -> p.toString().endsWith(".txt"))
+                    .collect(Collectors.toList());
+//                    .forEach(System.out::println);
+        }
+
+        System.out.println(pathList);
+
+        String resultFileName = dir.getParent().toString() + File.separator + dir.getFileName().toString() + "_joined.txt";
+        if (Files.exists(Paths.get(resultFileName))) {
+            Files.delete(Paths.get(resultFileName));
+        }
+
+        RandomAccessFile aFile = new RandomAccessFile(resultFileName, "rw");
+        FileChannel destChannel = aFile.getChannel();
+
+        long position = 0;
+        for (Path curPath : pathList) {
+            FileChannel sourceChannel = new FileInputStream(curPath.toString()).getChannel();
+            destChannel.transferFrom(sourceChannel, position, sourceChannel.size());
+            position += sourceChannel.size();
+//            System.out.println(position);
+            sourceChannel.close();
+        }
+        destChannel.close();
+
+        return Paths.get(resultFileName);
+    }
 
 }
