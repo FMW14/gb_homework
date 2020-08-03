bf0c8d481f0bea05e956a98bdc679f78d60eef6f
FMW14 <fmw14@yandex.ru>
2020-08-03 18:15:03 +0300

    repos done

--- src/main/java/com/vtb/javacourses/lesson21/repos/ProductRepo.java
+++ src/main/java/com/vtb/javacourses/lesson21/repos/ProductRepo.java
@@ -1,25 +1,27 @@
 package com.vtb.javacourses.lesson21.repos;
 
-import com.vtb.javacourses.lesson21.entities.User;
+import com.vtb.javacourses.lesson21.entities.Product;
 import com.vtb.javacourses.lesson21.util.HibernateUtil;
 import org.hibernate.Session;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class ProductRepo {
-    public User getById(Long id) {
+    public Product getById(Long id) {
 
         if (id == null) {
             throw new IllegalArgumentException("id cannot be null");
         }
 
         Session session = null;
-        User userFromDb;
+        Product productFromDb;
 
         try {
             session = HibernateUtil.getSession();
             session.beginTransaction();
-            userFromDb = session.get(User.class, id);
+            productFromDb = session.get(Product.class, id);
             session.getTransaction().commit();
 
         } finally {
@@ -28,17 +30,17 @@ public class ProductRepo {
             }
         }
 
-        return userFromDb;
+        return productFromDb;
     }
 
-    public List<User> getAll() {
+    public List<Product> getAll() {
         Session session = null;
-        List<User> userList;
+        List<Product> productList;
 
         try {
             session = HibernateUtil.getSession();
             session.beginTransaction();
-            userList = HibernateUtil.getSession().createQuery("from User").list();
+            productList = HibernateUtil.getSession().createQuery("from User").list();
             session.getTransaction().commit();
         } finally {
             if (session != null) {
@@ -46,6 +48,26 @@ public class ProductRepo {
             }
         }
 
-        return userList;
+        return productList;
+    }
+
+    public void save(Product product) {
+
+        if (product == null) {
+            throw new IllegalArgumentException("product cannot be null");
+        }
+
+        Session session = null;
+
+        try {
+            session = HibernateUtil.getSession();
+            session.beginTransaction();
+            session.saveOrUpdate(product);
+            session.getTransaction().commit();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
--- src/main/java/com/vtb/javacourses/lesson21/repos/UserRepo.java
+++ src/main/java/com/vtb/javacourses/lesson21/repos/UserRepo.java
@@ -1,7 +1,73 @@
 package com.vtb.javacourses.lesson21.repos;
 
+import com.vtb.javacourses.lesson21.entities.User;
+import com.vtb.javacourses.lesson21.util.HibernateUtil;
+import org.hibernate.Session;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class UserRepo {
+    public User getById(Long id) {
+
+        if (id == null) {
+            throw new IllegalArgumentException("id cannot be null");
+        }
+
+        Session session = null;
+        User userFromDb;
+
+        try {
+            session = HibernateUtil.getSession();
+            session.beginTransaction();
+            userFromDb = session.get(User.class, id);
+            session.getTransaction().commit();
+
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+        return userFromDb;
+    }
+
+    public List<User> getAll() {
+        Session session = null;
+        List<User> userList;
+
+        try {
+            session = HibernateUtil.getSession();
+            session.beginTransaction();
+            userList = HibernateUtil.getSession().createQuery("from User").list();
+            session.getTransaction().commit();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+        return userList;
+    }
+
+    public void save(User user) {
+
+        if (user == null) {
+            throw new IllegalArgumentException("user cannot be null");
+        }
+
+        Session session = null;
+
+        try {
+            session = HibernateUtil.getSession();
+            session.beginTransaction();
+            session.saveOrUpdate(user);
+            session.getTransaction().commit();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
 }
