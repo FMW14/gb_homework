71a84982c56d2c9c4803d5d1ace9cb9fb9762b92
FMW14 <fmw14@yandex.ru>
2020-08-04 14:52:04 +0300

    dependencies updated #2

--- src/main/java/com/vtb/javacourses/lesson18/exceptions/EmptyListException.java
+++
@@ -1,7 +0,0 @@
-package com.vtb.javacourses.lesson18.exceptions;
-
-public class EmptyListException extends RuntimeException {
-    public EmptyListException(String message) {
-        super(message);
-    }
-}
